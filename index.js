d8ad565e15551fa91e62fe13c3ba8174bc383976
notoriousrip <3935212+unimpossible@users.noreply.github.com>
2025-09-18 13:54:11 -0600

    use dotenv

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+require('dotenv').config()
+PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
