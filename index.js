ea041762824832bd9f00433ac664c0609e871d79
notoriousrip <3935212+unimpossible@users.noreply.github.com>
2025-09-18 15:03:59 -0600

    Add listing

--- index.js
+++ index.js
@@ -13,13 +13,26 @@ PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 app.get('/', async (req, res) => {
-    res.json({});
+    const customObjectUrl = "https://api.hubapi.com/crm/v3/objects/2-50257223?properties=address,asking_price,year_built";
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const resp = await axios.get(customObjectUrl, { headers });
+        const data = resp.data.results;
+        res.render("homepage", { data });
+    } catch (error) {
+        console.error(error);
+        res.json({ 'error': true });
+    }
 });
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 app.get('/update-cobj', async (req, res) => {
-    res.render("updates")
+    res.render("updates");
 });
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
