72e186c204bfb8756384be262c2209bf649b7503
notoriousrip <3935212+unimpossible@users.noreply.github.com>
2025-09-18 14:39:15 -0600

    Add updates page with form and create new custom object

--- index.js
+++ index.js
@@ -12,15 +12,40 @@ PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    res.json({});
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    res.render("updates")
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+app.post('/update-cobj', async (req, res) => {
+    const update = {
+        properties: {
+            "address": req.body.address,
+            "asking_price": req.body.asking_price,
+            "year_built": req.body.year_built
+        }
+    }
 
-// * Code for Route 3 goes here
+    const createObjectUrl = "https://api.hubapi.com/crm/v3/objects/2-50257223";
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try { 
+        await axios.post(createObjectUrl, update, { headers } );
+        res.redirect('/');
+    } catch(err) {
+        console.error(err);
+        res.json({'error':true});
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
